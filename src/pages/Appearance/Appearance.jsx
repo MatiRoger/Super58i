7ffd0807c3e4184ca6d1e4cbc3ad1054fea91c02
MauroGutierrez53 <mauro.5389@gmail.com>
2023-09-25 21:17:14 -0300

    pagina de error404 y appearance

--- src/pages/Appearance/Appearance.jsx
+++ src/pages/Appearance/Appearance.jsx
@@ -1,8 +1,49 @@
 import React from 'react'
+import './Appearance.css'
 
 const Appearance = () => {
   return (
-    <h1>Appearance</h1>
+    <>
+    <ul className='tab-body-single Appearance'>
+      <li>
+        <span>
+          <i className='fas fa-star'></i>gender
+        </span>
+        <span>Male</span>
+      </li>
+      <li>
+        <span>
+          <i className='fas fa-star'></i>race
+        </span>
+        <span>Human</span>
+      </li>
+      <li>
+        <span>
+          <i className='fas fa-star'></i>height
+        </span>
+        <span>5' 10''</span>
+      </li>
+      <li>
+        <span>
+          <i className='fas fa-star'></i>weight
+        </span>
+        <span>170 lb</span>
+      </li>
+      <li>
+        <span>
+          <i className='fas fa-star'></i>eye-color
+        </span>
+        <span>Blue</span>
+      </li>
+      <li>
+        <span>
+          <i className='fas fa-star'></i>hair-color
+        </span>
+        <span>Black</span>
+      </li>
+
+    </ul>
+    </>
   )
 }
 
--- src/pages/Error404/Error404.jsx
+++ src/pages/Error404/Error404.jsx
@@ -1,15 +1,20 @@
 import React from 'react'
+import './Error404.css'
+
 
 const Error404 = () => {
   return (
     <>
     
-    <h1>Error404</h1>
-    <div class="container">
-        <h1>404 - Página no encontrada</h1>
-        <p>Lo siento, la página que buscas no existe en el mundo de los superhéroes.</p>
-        <p><a href="/">Volver a la página de inicio</a></p>
+    <div className="container containerMG bodyMG">
+      <h1 className='tituloMG'>404</h1>
+
+        <h1 className='subtMG'>La página que ha solicitado no se encuentra disponible</h1>
+        <div className="boton buttonMG">
+            <button>Volver atras</button>
+        </div>
     </div>
+
 
     </>
   )
