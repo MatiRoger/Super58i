f85b5bfb8ab6295d86055ea093615c9516188766
Matias Roger Narcotti <matiasrogern.dev@gmail.com>
2023-09-25 22:09:12 -0300

    .

--- src/components/SectionNav/SectionNav.jsx
+++ src/components/SectionNav/SectionNav.jsx
@@ -3,20 +3,20 @@ import { Link } from 'react-router-dom'
 
 const SectionNav = () => {
   return (
-    <ul className="nav justify-content-end">
-    <li className="nav-item">
-       <Link className="nav-a active" aria-current="page" to="#">powerstats</Link>
-     </li>
-     <li className="nav-item">
-       <Link className="nav-a active" aria-current="page" to="#">biography</Link>
-     </li>
-     <li className="nav-item">
-       <Link className="nav-a active" aria-current="page" to="#">appearance</Link>
-     </li>
-     <li className="nav-item">
-       <Link className="nav-a active" aria-current="page" to="#">connections</Link>
-     </li>
-   </ul>
+    <ul className="sectionNav nav justify-content-center gap-3 mt-3 align-self-start">
+      <li className="nav-item">
+        <Link className="sectionLink" aria-current="page" to="/">POWERSTATS</Link>
+      </li>
+      <li className="nav-item">
+        <Link className="sectionLink" aria-current="page" to="/biography">BIOGRAPHY</Link>
+      </li>
+      <li className="nav-item">
+        <Link className="sectionLink" aria-current="page" to="/appearance">APPEARANCE</Link>
+      </li>
+      <li className="nav-item">
+        <Link className="sectionLink" aria-current="page" to="/connections">CONNECTIONS</Link>
+      </li>
+    </ul>
   )
 }
 
